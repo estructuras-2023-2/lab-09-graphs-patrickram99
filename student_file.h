75863b3b0c4e75c14194c1337528e59273aad6c1
ksajkdjaskjdsakjads <pramirezs@ulasalle.edu.pe>
2023-11-25 02:27:27 +0000

    Se añade implementacion con arbol de expansion minimo

--- student_file.h
+++ student_file.h
@@ -1,63 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Edge {
-    string id, u, v;
-    int cost;
-    bool operator<(const Edge& rhs) const {
-        if(cost != rhs.cost) return cost < rhs.cost;
-        return id < rhs.id;
+struct edge {
+    string id;
+    int u, v, cost;
+    bool operator<(const edge& e) const {
+        if(cost != e.cost) return cost < e.cost;
+        return id < e.id;
     }
 };
 
-string find(map<string, string>& parent, string i) {
-    if(parent[i] == i) return i;
-    return parent[i] = find(parent, parent[i]);
+vector<int> parent;
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
 }
 
-void Union(map<string, string>& parent, string x, string y) {
-    string xset = find(parent, x);
-    string yset = find(parent, y);
-    parent[xset] = yset;
+void make_set(int v) {
+    parent[v] = v;
 }
 
-string solve(vector<string> roads) {
-    vector<Edge> edges;
-    map<string, string> parent;
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+        parent[b] = a;
+}
+
+vector<string> reconstructRoads(vector<string> roads) {
+    map<string, int> city_to_int;
+    vector<edge> edges;
+    int city_counter = 0;
     for(string road : roads) {
         stringstream ss(road);
-        string id, u, v;
-        int cost = INT_MAX;
-        ss >> id >> u >> v;
-        if(ss >> cost);
-        else continue; // Skip the road if it's not damaged
-        edges.push_back({id, u, v, cost});
-        parent[u] = u;
-        parent[v] = v;
-    }
-    sort(edges.begin(), edges.end());
-    vector<string> res;
-    int totalCost = 0;
-    for(Edge e : edges) {
-        string x = find(parent, e.u);
-        string y = find(parent, e.v);
-        if (x != y) {
-            res.push_back(e.id);
-            totalCost += e.cost;
-            Union(parent, x, y);
+        string id, city1, city2;
+        int cost = -1;
+        ss >> id >> city1 >> city2;
+        if(ss >> cost) {
+            if(city_to_int.find(city1) == city_to_int.end()) city_to_int[city1] = city_counter++;
+            if(city_to_int.find(city2) == city_to_int.end()) city_to_int[city2] = city_counter++;
+            edges.push_back({id, city_to_int[city1], city_to_int[city2], cost});
         }
     }
-    for(auto it = parent.begin(); it != parent.end(); it++)
-        if(find(parent, it->first) != find(parent, parent.begin()->first))
-            return "IMPOSIBLE";
-    sort(res.begin(), res.end());
-    string result = "";
-    for(string id : res) result += id + " ";
-    return result.substr(0, result.size()-1);
+    sort(edges.begin(), edges.end());
+    parent.resize(city_counter);
+    for(int i = 0; i < city_counter; i++) make_set(i);
+    vector<string> result;
+    for(auto e : edges) {
+        if(find_set(e.u) != find_set(e.v)) {
+            result.push_back(e.id);
+            union_sets(e.u, e.v);
+        }
+    }
+    for(int i = 1; i < city_counter; i++) {
+        if(find_set(i) != find_set(0)) {
+            result.clear();
+            result.push_back("IMPOSIBLE");
+            break;
+        }
+    }
+    return result;
 }
 
 int main() {
-    vector<string> roads = {"T1 Tumbes Quito", "C1 Iquitos StMartin 1000"};
-    cout << solve(roads) << endl;
+    vector<string> roads = {"C1 Lima Trujillo 1", "C2 Tacna Trujillo", "C3 Tacna Arequipa"};
+    vector<string> result = reconstructRoads(roads);
+    for(string id : result) cout << id << " ";
+    cout << endl;
     return 0;
+    
 }
